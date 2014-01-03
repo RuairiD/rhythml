be9412e0b0811d49b2dfc3fe00e9a6d050ac7d72
Ruairi Dorrity <ruairidorrity@gmail.com>
2014-01-03 14:56:32 +0000

    Rhythm maps are stored as a global ref and can be played and looped. The ref can be updated while playing to allow a new rhythm to be played.

--- src/rhythml/core.clj
+++ src/rhythml/core.clj
@@ -2,36 +2,61 @@
   (:use [overtone.live]
 		 [overtone.inst.drum]))
   
-(defn bd [] (overtone.inst.drum/kick))
-(defn ch [] (overtone.inst.drum/closed-hat))
-(defn sn [] (overtone.inst.drum/snare 405 1 0.1 0.1 0.25 40 1000))
+(defn bd [] (kick))
+(defn ch [] (closed-hat))
+(defn sn [] (snare 405 1 0.1 0.1 0.25 40 1000))
 
 (def drumPool (overtone.at-at/mk-pool))
 
 (def rhy {
-	:interval 200, 
+	:interval 200,
+	:length 4, 
 	:beats [
 		{:count 0, :instruments [bd ch]}
 		{:count 1, :instruments [ch]}
 		{:count 2, :instruments [ch sn]}
+		{:count 3, :instruments [ch]}]})
+
+(def rhy2 {
+	:interval 200,
+	:length 4, 
+	:beats [
+		{:count 0, :instruments [bd ch]}
+		{:count 1, :instruments [bd]}
+		{:count 2, :instruments []}
+		{:count 3, :instruments [bd]}]})
+
+(def rhy3 {
+	:interval 100,
+	:length 8,
+	:beats [
+		{:count 0, :instruments [bd ch]}
+		{:count 1, :instruments [ch]}
+		{:count 2, :instruments [ch]}
 		{:count 3, :instruments [ch]}
-	],
-	:length 4
-})
+		{:count 4, :instruments [ch sn]}
+		{:count 5, :instruments [ch]}
+		{:count 6, :instruments [ch]}
+		{:count 7, :instruments [ch]}
+		{:count 8, :instruments [ch]}]})
+
+(def rhyRef (ref rhy))
+	
+(defn updateRhythm [r]
+	(dosync (ref-set rhyRef r)))
 
 (defn playBeat [beat]
-	(for [ins beat]
-		(ins)
-	)
-)
+	(dorun (for [ins beat]
+		(ins))))
 
-(defn playRhythm [r]
-	(let [time (now), beats (get r :beats), interval (get r :interval)]
-		(for [beat beats]
-			(apply-at (+ (* interval (get beat :count)) time) playBeat [(get beat :instruments)])
-		)
-	)
-)
+(defn playRhythm [rRef]
+	(let [time (now), r (deref rRef), beats (get r :beats), interval (get r :interval), length (get r :length)]
+		(do 
+			(dorun (for [beat beats]
+				(apply-at (+ (* interval (get beat :count)) time) playBeat [(get beat :instruments)])))
+			(apply-at (+ (* length interval) time) playRhythm [rRef]))))
 
-(defn loopRhythm [r] 
-    (overtone.at-at/every (* (get r :length) (get r :interval)) #(playRhythm r) drumPool))
+(defn startRhythm [r] 
+	(do
+		(updateRhythm r)
+		(playRhythm rhyRef)))
