b3b80e00532b4900d7397b24fc01e105ada54e33
Ruairi Dorrity <ruairidorrity@gmail.com>
2014-02-04 20:42:46 +0000

    Added HttpKit server and Javascript client to send Sticks from a browser using Websockets. RhythML and Sticks can now be used without writing pure Clojure.

--- src/rhythml/core.clj
+++ src/rhythml/core.clj
@@ -1,6 +1,28 @@
 (ns rhythml.core
-  (:use [rhythml.skins] [rhythml.sticks]))
+  (:use [rhythml.skins] [rhythml.sticks] [org.httpkit.server]))
   
 (defn read-rhythm [s] (read-sticks s))
 
+(defn receive-rhythm [data]
+	(do
+		(read-rhythm data)))
+
+(defn handler [req]
+  (with-channel req channel              ; get the channel
+    ;; communicate with client using method defined above
+    (on-close channel (fn [status]
+                        (println "Rhythm Channel Closed.")))
+    (if (websocket? channel)
+      (println "WebSocket channel opened.")
+      (println "HTTP channel opened."))
+    (on-receive channel (fn [data]
+                          (receive-rhythm data))))) 
+						  
+(defn start-server [options]
+	(let [port (options :port)]
+		(do
+			(run-server handler options)
+			(println "Sticks server started on port " port "."))))
+
+(start-server {:port 8080})
 (start-rhythm)
